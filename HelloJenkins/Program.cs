e5a87568f0f685929bdbff537883ed5489fbd03b
chavp <my.parinya@gmail.com>
2012-09-19 14:28:37 +0700

    add file cs
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ HelloJenkins/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Hello Jenkins!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
