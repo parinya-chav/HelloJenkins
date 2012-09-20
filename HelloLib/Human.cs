202b93a2343f9549b9ff7607438619f4d2d28838
chavp <my.parinya@gmail.com>
2012-09-20 13:38:02 +0700

    add Human Lib
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ HelloLib/Human.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloLib
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public string WriteHello()
+        {
+            return "Hi, I am " + Name;
+        }
+    }
+}
