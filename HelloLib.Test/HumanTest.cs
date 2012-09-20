202b196ef7e73ec84c8ec62b857f884786e97aed
chavp <my.parinya@gmail.com>
2012-09-20 14:09:22 +0700

    unittest use MbUnit
    Signed-off-by: chavp <my.parinya@gmail.com>

--- HelloLib.Test/HumanTest.cs
+++ HelloLib.Test/HumanTest.cs
@@ -2,14 +2,14 @@
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HelloLib.Test
 {
-    [TestClass]
+    using MbUnit.Framework;
+
     public class HumanTest
     {
-        [TestMethod]
+        [Test]
         public void TestWriteHello()
         {
             var human = new Human
