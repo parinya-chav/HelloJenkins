99abbba58eb2ea666648e2ea0a4d613530a99639
chavp <my.parinya@gmail.com>
2012-09-20 13:53:00 +0700

    add UnitTest
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ HelloLib.Test/HumanTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloLib.Test
+{
+    [TestClass]
+    public class HumanTest
+    {
+        [TestMethod]
+        public void TestWriteHello()
+        {
+            var human = new Human
+            {
+                Name = "Ding",
+            };
+
+            Assert.AreEqual("Hi, I am Ding", human.WriteHello());
+        }
+    }
+}
